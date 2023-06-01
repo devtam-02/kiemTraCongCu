b803b2be68fe43d7c81992d7ead85d3ee3144036
unknown <nguyenkiet228@gmail.com>
2023-06-01 10:11:23 +0700

    first commit

---
+++ src/main/java/com/example/ktCongCu/KtCongCuApplication.java
@@ -0,0 +1,13 @@
+package com.example.ktCongCu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtCongCuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KtCongCuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ktCongCu/KtCongCuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ktCongCu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtCongCuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
